fed0cdf2de7e47496e62fecc9188e24cb1e7781e
j231043 <j23041043@student.newinti.edu.my>
2026-04-25 15:56:45 +0800

    copy ex0.2 to ex3

---
+++ Exercise 3/energy-webpage-v1/js/scripts.js
@@ -0,0 +1,70 @@
+
+// setting up variable for elements in HTML page
+const button_nav_home = document.querySelector("#nav_home");
+const button_nav_tv = document.querySelector("#nav_tv");
+const button_nav_abt = document.querySelector("#nav_abt");
+const main = document.querySelector("main");
+
+// content to be inserted into html main tag
+const content_home = `
+    <h1>Welcome to Australia Appliance Energy Consumption</h1>
+    <p>Here you can find the energy consumption rate of common household appliances in Australia. The list is update monthly.</p>
+
+    <h2>Appliance Energy Consumption in Australia</h2>
+    <p>Household appliances and equipment account for an average of 25% of total residential energy consumption across Australia. However, this proportion will vary by household depending on the climate, the types of appliances in your home, and the way they are used. Heating and cooling uses around 40% of household energy use.
+    <br>
+    Appliances that use the largest amounts of energy include fridges and freezers (responsible for an average 7% of household energy use), clothes dryers (up to 10% of household energy use for heavy users), and TVs and home entertainment equipment (an average of around 5% of household energy use). In homes with a pool, the pool pump is a high user of energy (up to 18%).
+    <br>
+    Household appliances contribute to peak electricity demand, which refers to major spikes in electricity use that occurs at certain times (for example, between 5pm and 8pm when people arrive home from work and turn on their air-conditioners, TVs, lights, and other appliances). If peak demand exceeds the maximum supply levels, some regions can experience electricity outages. Supplying electricity for an ever-increasing peak demand requires building more electricity infrastructure, which is paid for by increases in energy prices.
+    </p>
+    <p>Source: https://www.yourhome.gov.au/energy/appliances</p>
+`;
+
+const content_tv = `
+    <h1>TV</h1>
+    <p>Here you can find the energy consumption rate of appliances in Australia. The list is update annually.</p>
+`;
+
+const content_abt = `
+    <h1>About us</h1>
+    <p>We are the Australia Consumer Group.</p>
+`;
+
+// function to highlight current page on the navigation menu
+function display_current_page(current){
+    button_nav_home.style.backgroundColor = "";
+    button_nav_tv.style.backgroundColor = "";
+    button_nav_abt.style.backgroundColor = "";
+    current.style.backgroundColor = "#f6f5b4";
+
+}
+
+// function to insert content into html page 
+function display_home() {
+    main.innerHTML = content_home;
+    display_current_page(button_nav_home);
+};
+
+function display_tv() {
+    main.innerHTML = content_tv;
+    display_current_page(button_nav_tv);
+
+};
+
+function display_abt() {
+    main.innerHTML = content_abt;
+    display_current_page(button_nav_abt);
+
+};
+
+
+// event listeners for each page on the navigation menu
+button_nav_home.addEventListener("click", display_home)
+button_nav_tv.addEventListener("click", display_tv)
+button_nav_abt.addEventListener("click", display_abt)
+
+// default behaviour when page is first loaded is display homepage
+display_home();
+
+
+
