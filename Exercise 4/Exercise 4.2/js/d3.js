a545a65ae845000fd11869408f15b19ecae5826e
j231043 <j23041043@student.newinti.edu.my>
2026-04-27 13:48:35 +0800

    append element and svg

--- Exercise 4/Exercise 4.2/js/d3.js
+++ Exercise 4/Exercise 4.2/js/d3.js
@@ -1,2 +1,13 @@
 d3.selectAll("h1").style("color", "green");
 d3.selectAll("p").style("color", "blue");
+d3.select("div")
+  .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+d3.select("svg")
+  .append("rect")
+   .attr("x", 50)
+   .attr("y", 50)
+   .attr("width", 100)
+   .attr("height", 30)
+   .style("fill", "green");
