4ea1c7f8e50bfbb11eedd3e829046f205c0844c8
j231043 <j23041043@student.newinti.edu.my>
2026-04-27 14:02:07 +0800

    change svg to responsive viewbox

--- Exercise 4/Exercise 4.2/js/d3.js
+++ Exercise 4/Exercise 4.2/js/d3.js
@@ -4,6 +4,11 @@ d3.select("div")
   .append("p")
     .text("Purchasing a low energy consumption TV will help with your energy bills!");
 
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+        .attr("viewBox", "0 0 1200 1600")
+            .style("border", "1px solid black");
+
 d3.select("svg")
   .append("rect")
    .attr("x", 50)
