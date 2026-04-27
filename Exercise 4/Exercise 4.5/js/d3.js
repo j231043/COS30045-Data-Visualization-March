3b1621e1da12ce6802b3f39f052042a0d48db310
j231043 <j23041043@student.newinti.edu.my>
2026-04-27 16:48:03 +0800

    ex4.5 step 1

---
+++ Exercise 4/Exercise 4.5/js/d3.js
@@ -0,0 +1,37 @@
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+      .attr("viewBox", "0 0 1200 1600")
+      .style("border", "1px solid black");
+
+// svg
+//   .append("rect")
+//     .attr("x", 10)
+//     .attr("y", 10)
+//     .attr("width", 414)
+//     .attr("height", 16)
+//     .attr("fill", "blue");
+
+
+d3.csv("/Exercise 4/Exercise 4.4/data/data.csv", d => {
+  // console.log(d); 
+  return {
+    Screen_Tech: d.Screen_Tech,
+    Avg_mode_power: +d.Avg_mode_power
+  };
+}).then(data => {
+  console.log(data);
+  createBarChart(data);
+  }
+);
+
+const createBarChart = data => {
+  svg
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("class", d => {
+    console.log(d);
+    return `bar bar-${d.Avg_mode_power}`;
+   })
+};
+
