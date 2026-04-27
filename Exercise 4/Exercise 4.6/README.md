cef313335a27a200bb3991b62fc9d40a8a5edb62
j231043 <j23041043@student.newinti.edu.my>
2026-04-27 17:03:03 +0800

    preparation task

--- Exercise 4/Exercise 4.6/README.md
+++ Exercise 4/Exercise 4.6/README.md
@@ -1,1 +1,10 @@
-# Exercise 4.6
+# AI USAGE
+
+1. question 1
+PROMPTS USED:
+
+
+OUTPUT RECEIVED:
+
+
+MODIFICATIONS MADE: no modifications made, hardcoded path to data.csv
---
+++ Exercise 4/Exercise 4.6/js/d3.js
@@ -0,0 +1,42 @@
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+      .attr("viewBox", "0 0 100 1600")
+      .style("border", "1px solid black");
+
+// svg
+//   .append("rect")
+//     .attr("x", 10)
+//     .attr("y", 10)
+//     .attr("width", 414)
+//     .attr("height", 16)
+//     .attr("fill", "blue");
+
+
+d3.csv("/Exercise 4/Exercise 4.5/data/data.csv", d => {
+  // console.log(d); 
+  return {
+    Screen_Tech: d.Screen_Tech,
+    Avg_mode_power: +d.Avg_mode_power
+  };
+}).then(data => {
+  console.log(data);
+  createBarChart(data);
+  }
+);
+const barHeight = 50;
+
+const createBarChart = data => {
+  svg
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("class", d => {
+    console.log(d);
+    return `bar bar-${d.Avg_mode_power}`;
+   })
+  .attr("width", d => d.Avg_mode_power)
+  .attr("height", barHeight)
+  .attr("x", 0)
+  .attr("y", (d, i) => (barHeight + 5) * i);
+};
+
