237c4ac01898a7442480951a8efdba25579c3fdc
j231043 <j23041043@student.newinti.edu.my>
2026-05-18 12:09:59 +0800

    minor edits to svg location and text

--- Exercise 4/Exercise 4.4/js/d3.js
+++ Exercise 4/Exercise 4.4/js/d3.js
@@ -1,3 +1,7 @@
+d3.select("div ")
+  .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
 const svg = d3.select(".responsive-svg-container")
     .append("svg")
       .attr("viewBox", "0 0 1200 1600")
--- Exercise 4/Exercise 4.5/js/d3.js
+++ Exercise 4/Exercise 4.5/js/d3.js
@@ -1,4 +1,8 @@
-const svg = d3.select(".responsive-svg-container")
+d3.select("div ")
+  .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+    const svg = d3.select(".responsive-svg-container")
     .append("svg")
       .attr("viewBox", "0 0 1200 1600")
       .style("border", "1px solid black");
